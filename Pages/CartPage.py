dd762276e23530dcb9d49ffdce4c51abbfacb986
G-Kiruthika <88583981+G-Kiruthika@users.noreply.github.com>
2026-02-06 02:44:13 +0530

    Deploying Page Objects

--- Pages/CartPage.py
+++ Pages/CartPage.py
@@ -1,6 +1,7 @@
 # imports
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.common.exceptions import NoSuchElementException
 
 class CartPage:
     def __init__(self, driver: WebDriver):
@@ -9,6 +10,9 @@ class CartPage:
         self.cart_item_quantity = (By.XPATH, "//div[@class='cart-item' and @data-product-id='12345']//input[@class='cart-quantity']")
         self.cart_error_message = (By.CSS_SELECTOR, '.cart-error-message')
         self.stock_warning_message = (By.XPATH, "//div[contains(@class, 'stock-warning') or contains(@class, 'error-message')]")
+        # Added selectors for cart deletion and verification
+        self.delete_cart_button = (By.XPATH, "//button[@id='delete-cart' and @data-cart-id='{cart_id}']")
+        self.cart_container = (By.XPATH, "//div[@class='cart-container' and @data-cart-id='{cart_id}']")
 
     def open_cart(self):
         self.driver.find_element(*self.cart_icon).click()
@@ -27,3 +31,31 @@ class CartPage:
             return self.driver.find_element(*self.stock_warning_message).text
         except:
             return None
+
+    # --- ADDED for TC_CART_007 ---
+    def delete_cart(self, cart_id: str):
+        """
+        Deletes the shopping cart for the given cart_id.
+        """
+        try:
+            delete_button_locator = (self.delete_cart_button[0], self.delete_cart_button[1].format(cart_id=cart_id))
+            self.driver.find_element(*delete_button_locator).click()
+            # Optionally, handle confirmation dialog
+            try:
+                confirm_button = self.driver.find_element(By.XPATH, "//button[@id='confirm-delete-cart']")
+                confirm_button.click()
+            except NoSuchElementException:
+                pass
+        except NoSuchElementException:
+            raise Exception(f"Delete button for cart_id {cart_id} not found.")
+
+    def is_cart_deleted(self, cart_id: str) -> bool:
+        """
+        Returns True if the cart is deleted (not found), False otherwise.
+        """
+        try:
+            cart_locator = (self.cart_container[0], self.cart_container[1].format(cart_id=cart_id))
+            self.driver.find_element(*cart_locator)
+            return False
+        except NoSuchElementException:
+            return True
